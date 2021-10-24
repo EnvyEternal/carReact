7953057578448d5078470244d625127042115c95
E Eternal <eeternal@Es-MacBook-Air.lan>
2021-10-24 12:57:07 +0300

    $

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import './App.css';
 import React from 'react';
 import UsersList from './modules/UsersList';
 import Car from './modules/Car'
-import Car2 from './modules/Car2'
+
  
 const App = () => 
 <>
--- src/modules/Car2/index.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-
-export default function car() {
-    return (
-        <div>
-            1
-        </div>
-    )
-}
